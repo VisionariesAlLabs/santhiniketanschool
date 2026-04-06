fcdbab7c288852a170a1b791b4c5cc2d702c9d03
unknown <ippililalith@gmail.com>
2026-04-06 15:26:53 +0530

    setup github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/santhiniketanschool/", // replace with your exact GitHub repo name
+  base: "/", // replace with your exact GitHub repo name
   server: {
     host: "::",
     port: 8080,
